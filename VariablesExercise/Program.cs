2402e86e37140883efb0bedfcb9124df6547e4d3
Zachary Guesmia <zguesmia0136@gmail.com>
2025-01-05 23:45:56 -0600

    Assignment complete.

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Zach";
+            int age = 28;
+            char courseGrade = 'A';
+            bool truth = true;
+            double courseGrade2 = 92.1;
+
+            Console.WriteLine("Hello, my name is " + name);
+            Console.WriteLine("I am " + age + " years old.");
+            Console.WriteLine("Last semester, it is " + truth + "I took two courses and got an " + courseGrade + " in each one.");
+            Console.WriteLine("In my Spanish class, my grade was a " +  courseGrade2);
         }
     }
 }
